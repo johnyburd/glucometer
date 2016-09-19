aef059dbda5c447fa661906861fbe31683be1809
Jonathan Buchanan <johnyburd8@gmail.com>
2016-09-19 16:50:13 -0400

    for testing

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ##Glucometer UI Code
-This is the code for the user interface of a glucose meter I am building.  It requires python 2.7 and [kivy](https://kivy.org/#home).
+This is the code for the user interface of a glucose meter I am building.  It requires python 2.7, [kivy](https://kivy.org/#home), and [sqlite](https://www.sqlite.org).
 Note that the project page in on [hackaday](https://hackaday.io/project/12778-raspberry-pi-glucometer), this is simply the code.
 
 ###License
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.actionbar import ActionBar
+from kivy.uix.dropdown import DropDown
+from kivy.uix.button import Button
 from kivy.properties import StringProperty
 from time import gmtime, strftime
 import FlappyBird
@@ -20,19 +22,31 @@ class SettingsScreen(Screen):
 
 class ExtrasScreen(Screen):
     pass
+class HomeScreen(Screen):
+    pass
 class MenuBar(ActionBar):
     def set_previous(self):
-        if sm.current_screen and sm.current_screen.name == 'data':
+        if sm.current_screen and sm.current_screen.name == 'home':
             return True
         return False
     def get_time(self):
-       return strftime("%Y-%m-%d %H:%M", gmtime()) 
+       return strftime("%Y-%m-%d %H:%M", gmtime())
+
+
+class CustomDropDown(DropDown):
+    pass
+
+dropdown = CustomDropDown()
+mainbutton = Button(text='Hello', size_hint=(None, None))
+mainbutton.bind(on_release=dropdown.open)
+dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
 
 
 kvfile = Builder.load_file("glucometer.kv")
 
 sm = ScreenManager()
 
+sm.add_widget(HomeScreen(name='home'))
 sm.add_widget(DataScreen(name='data'))
 sm.add_widget(MainScreen(name='main'))
 sm.add_widget(BGScreen(name='bgtest'))
@@ -52,6 +66,8 @@ class Glucometer(App):
         return StringProperty('test')
     def flappy(self):
         FlappyBird.FlappyBirdApp().run()
+    def opendropdown(self):
+        dropdown.open
 
 
 Glucometer().run()
